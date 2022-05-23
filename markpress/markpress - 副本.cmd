6f524a4cb922208d3f2b1e7f9c32d4dc317499c9
htp168 <htp168@qq.com>
2022-05-23 12:21:40 +0800

    upload

--- markpress/markpress - 副本.cmd
+++ markpress/markpress - 副本.cmd
@@ -1,8 +1,8 @@
 @echo off
 rem 设置hexo目录路径
-set hexo_path="C:\Users\Administrator\Program\blog\source\_posts\"
+set hexo_path=C:\Users\Administrator\Program\blog\source\_posts\
 rem 设置python虚拟环境目录路径
-set pyenv_path="C:\Program Files (x86)\Tools\Sublime Text\Data\Packages\User\markpress\venv\Scripts\"
+set pyenv_path=C:\Program Files (x86)\Tools\Sublime Text\Data\Packages\User\pycnblog\venv\Scripts\
 rem 获取参数markdown文件路径
 set file_path=%1
 
@@ -10,7 +10,8 @@ set file_path=%1
 rem 校验文件类型是否markdown
 if not %~x1==.md (
     echo 文件类型不符，操作失败
-    goto :quit
+    pause>nul
+    exit
 )
 
 rem 检测markdown文件是否已存在
@@ -24,11 +25,11 @@ if not exist %pyenv_path% pip list --format freeze|find "pyyaml" || goto :pyenv
 
 rem 同步cnblog
 echo 同步cnblog
-if exist %pyenv_path% call "%~dp0\venv\Scripts\activate" && python print.py "%file_path%" && call "%~dp0\venv\Scripts\deactivate" && echo cnblog执行成功
+if exist %pyenv_path% call "%~dp0\venv\Scripts\activate" && python upload.py "%file_path%" && call "%~dp0\venv\Scripts\deactivate" && echo cnblog执行成功
 
 rem 同步hexo
 echo 同步hexo
-if exist %hexo_path% copy %~f1 %hexo_path%%~nx1 && echo hexo执行成功
+if exist %hexo_path% copy %~f1 %hexo_path%%~nx1 && hexo cl & hexo g -d && echo hexo执行成功
 
 :quit
 rem 退出
